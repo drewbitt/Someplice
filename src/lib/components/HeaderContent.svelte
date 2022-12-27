eca8baa69f1deaceeb728d339e470c950b6696ae
drewbitt <andrew@drewbitt.com>
2022-12-27 13:55:38 -0600

    Add header

---
+++ src/lib/components/HeaderContent.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Group, ActionIcon, Text, Burger, Tooltip } from '@svelteuidev/core';
+	import { hotkey, useOs } from '@svelteuidev/composables';
+	const os = useOs();
+	import { Sun, Moon } from 'radix-icons-svelte';
+	const mod = os === 'macos' ? '⌘' : 'ctrl';
+	export let isDark: boolean;
+	export let opened: boolean;
+	export let toggle: () => void;
+	export let toggleOpen: () => void;
+</script>
+
+<Group override={{ height: '100%', px: 20 }} position="apart">
+	<Burger {opened} on:click={toggleOpen} override={{ d: 'block', '@sm': { d: 'none' } }} />
+	<a href="/" style="text-decoration: none;">
+		<Group>
+			<!-- <Logo size={35} /> -->
+			<Text color="blue" size="xl" override={{ d: 'none', '@sm': { d: 'block' } }}>Someplice</Text>
+		</Group>
+	</a>
+	<Tooltip label={`${mod} + J`}>
+		<ActionIcon variant="default" on:click={toggle} size={30} use={[[hotkey, [['mod+J', toggle]]]]}>
+			{#if isDark}
+				<Moon />
+			{:else}
+				<Sun />
+			{/if}
+		</ActionIcon>
+	</Tooltip>
+</Group>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,47 @@
 <script>
-	import '../app.postcss';
+	// import '../app.postcss';
+	import {
+		fns,
+		AppShell,
+		Navbar,
+		Header,
+		Title,
+		Divider,
+		SvelteUIProvider,
+		Container,
+		ShellSection
+	} from '@svelteuidev/core';
+	import HeaderContent from '$lib/components/HeaderContent.svelte';
+
+	let isDark = false;
+	let opened = false;
+
+	function toggleTheme() {
+		isDark = !isDark;
+	}
+	function toggleOpened() {
+		opened = !opened;
+	}
 </script>
 
-<slot />
+<SvelteUIProvider withNormalizeCSS withGlobalStyles>
+	<AppShell
+		override={{
+			main: {
+				bc: isDark ? fns.themeColor('dark', 8) : fns.themeColor('gray', 0),
+				color: isDark ? fns.themeColor('dark', 0) : 'black',
+				ml: '0px !important'
+			}
+		}}
+	>
+		<Header
+			slot="header"
+			height={60}
+			override={{ p: '$mdPX', bc: isDark ? fns.themeColor('dark', 7) : 'white' }}
+		>
+			<HeaderContent {isDark} {opened} toggle={toggleTheme} toggleOpen={toggleOpened} />
+		</Header>
+
+		<slot />
+	</AppShell>
+</SvelteUIProvider>
