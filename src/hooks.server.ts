29dede0f383ad308e63cc4d5eab53651ba1317fd
drewbitt <andrew@drewbitt.com>
2023-01-21 20:33:06 -0500

    refactor: Improve ESM migration provider file pathing, replace server hook with new svelteui equivalent

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,23 +1,12 @@
-import { getCssText } from '@svelteuidev/core';
-import type { Handle } from '@sveltejs/kit';
 import { createContext } from '$lib/trpc/context';
 import { router } from '$lib/trpc/router';
-import { createTRPCHandle } from 'trpc-sveltekit';
+import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+import { prepareStylesSSR } from '@svelteuidev/core';
+import { createTRPCHandle } from 'trpc-sveltekit';
 
 export const trpcHandle: Handle = createTRPCHandle({ router, createContext });
 
-export const sveltuiHandle = (async ({ event, resolve }) => {
-	return await resolve(event, {
-		transformPageChunk: ({ html }: { html: string }): string => {
-			const headEndIndex = html.indexOf('</head>');
-			const returnHtml = `${html.slice(
-				0,
-				headEndIndex
-			)}<style id="stitches">${getCssText()}</style>${html.slice(headEndIndex)}`;
-			return returnHtml;
-		}
-	});
-}) satisfies Handle;
+export const sveltuiHandle = prepareStylesSSR;
 
 export const handle = sequence(trpcHandle, sveltuiHandle);
