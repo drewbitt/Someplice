08dc8863fc577b8b0b38330936dd73d4c9eb3903
drewbitt <andrew@drewbitt.com>
2023-03-12 22:16:22 -0400

    Linux update

--- src/lib/db/migrate-to-latest.ts
+++ src/lib/db/migrate-to-latest.ts
@@ -1,8 +1,10 @@
 import { promises as fs } from 'fs';
 import { Kysely, Migrator, type Migration, type MigrationProvider } from 'kysely';
-import { pathToFileURL } from 'url';
+import { pathToFileURL, fileURLToPath } from 'url';
 import type { DB } from '../types/data';
 import { db as mainDb } from './db';
+import os from 'os';
+import path from 'path';
 
 export async function migrateToLatest(db?: Kysely<DB>) {
 	if (!db) {
@@ -12,18 +14,37 @@ export async function migrateToLatest(db?: Kysely<DB>) {
 	const ViteMigrationProvider: MigrationProvider = {
 		async getMigrations() {
 			const migrations: Record<string, Migration> = {};
-			const files = await fs.readdir('./src/lib/db/migrations');
+			if (os.platform() === 'win32') {
+				const files = await fs.readdir('./src/lib/db/migrations');
 
-			for (const file of files) {
-				if (file.endsWith('.ts') && !file.endsWith('.d.ts')) {
-					// Hacky path conversion that works in Windows and probably only Windows
-					const filePath = pathToFileURL('./src/lib/db/migrations/' + file).href.replace(
-						'file://',
-						''
-					);
-					console.log('🚀 ~ file: migrate-to-latest.ts:22 ~ getMigrations ~ filePath', filePath);
-					const migration = await import(/* @vite-ignore */ filePath);
-					const migrationKey = file.substring(0, file.lastIndexOf('.'));
+				for (const file of files) {
+					if (file.endsWith('.ts') && !file.endsWith('.d.ts')) {
+						// Hacky path conversion that works in Windows and probably only Windows
+						const filePath = pathToFileURL('./src/lib/db/migrations/' + file).href.replace(
+							'file://',
+							''
+						);
+						console.log('Running migration ', filePath);
+						const migration = await import(/* @vite-ignore */ filePath);
+						const migrationKey = file.substring(0, file.lastIndexOf('.'));
+						migrations[migrationKey] = migration;
+					}
+				}
+			} else if (os.platform() === 'linux' || os.platform() === 'darwin') {
+				const __dirname = fileURLToPath(new URL('.', import.meta.url));
+				const resolvedPath = path.resolve(__dirname, 'migrations');
+				const files = await fs.readdir(resolvedPath);
+
+				for (const fileName of files) {
+					if (!fileName.endsWith('.js') && !fileName.endsWith('.ts')) {
+						continue;
+					}
+
+					const importPath = path.join(resolvedPath, fileName).replaceAll('\\', '/');
+					console.log('Running migration ', importPath);
+					const migration = await import(importPath);
+					const migrationKey = fileName.substring(0, fileName.lastIndexOf('.'));
+
 					migrations[migrationKey] = migration;
 				}
 			}
