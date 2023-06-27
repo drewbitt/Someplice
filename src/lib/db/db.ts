70b459a633b6556446730fdd9052cef8e30b7653
drewbitt <andrew@drewbitt.com>
2023-06-26 20:14:03 -0400

    fix: update regex method for DB

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -2,8 +2,16 @@ import Database from 'better-sqlite3';
 import { Kysely, SqliteDialect } from 'kysely';
 import type { DB } from '../types/data';
 
+const betterSqlite3 = new Database('./src/lib/db/db.sqlite');
+// Define REGEXP function
+betterSqlite3.function('regexp', { deterministic: true }, (regex: unknown, text: unknown) => {
+	if (typeof regex === 'string' && typeof text === 'string') {
+		return new RegExp(regex).test(text) ? 1 : 0;
+	}
+});
+
 export const db = new Kysely<DB>({
 	dialect: new SqliteDialect({
-		database: new Database('./src/lib/db/db.sqlite')
+		database: betterSqlite3
 	})
 });
