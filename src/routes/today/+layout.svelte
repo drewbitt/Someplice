4df888a9a01f8eb51cc8dc16cbf99c1c9ea2cbce
drewbitt <andrew@drewbitt.com>
2023-09-08 20:32:01 -0400

    fix: add missing layout update

--- src/routes/today/+layout.svelte
+++ src/routes/today/+layout.svelte
@@ -1,1 +1,20 @@
+<script lang="ts">
+	import { todayPageErrorStore } from '$src/lib/stores/errors';
+	import X from 'virtual:icons/lucide/x';
+</script>
+
+{#if $todayPageErrorStore}
+	<div class="daisy-toast daisy-toast-center">
+		<div class="daisy-alert daisy-alert-error flex gap-2">
+			<button
+				class="daisy-btn daisy-btn-circle daisy-btn-sm"
+				on:click={() => todayPageErrorStore.setError(null)}
+			>
+				<X />
+			</button>
+			<p>{$todayPageErrorStore}</p>
+		</div>
+	</div>
+{/if}
+
 <slot />
