d1c5fe93670a6d5766fe0f8d169b18c47dacddf7
drewbitt <andrew@drewbitt.com>
2023-09-11 07:58:23 -0400

    feat: implement pagination in Journey page

--- src/routes/journey/+page.svelte
+++ src/routes/journey/+page.svelte
@@ -1,17 +1,105 @@
 <script lang="ts">
 	import JourneyDayBox from '$src/lib/components/journey/JourneyDayBox.svelte';
 	import GoalBadges from '$src/lib/components/today/GoalBadges.svelte';
-	import { Notification, Title, colorScheme } from '@svelteuidev/core';
+	import { Loader, Notification, Title, colorScheme } from '@svelteuidev/core';
 	import CircleX from 'virtual:icons/lucide/x-circle';
 	import type { PageServerData } from './$types';
 	import EmptyDayBoxWrapper from '$src/lib/components/journey/EmptyDayBoxWrapper.svelte';
+	import { trpc } from '$src/lib/trpc/client';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
 
 	export let data: PageServerData;
+
+	// Reactive variables
 	$: noIntentions = Object.keys(data.intentionsByDate).length === 0;
 	$: noGoals = data.goals.length === 0;
-
 	$: darkMode = $colorScheme === 'dark';
-	let dates = Object.keys(data.intentionsByDate);
+	$: dates = Object.keys(data.intentionsByDate);
+
+	// Pagination parameters
+	/* 	
+	TODO: Due to invalidateAll(), the page will be de-paginated on changes in the JourneyDayBoxes.
+	This is not ideal, but it would be complicated to re-run all client queries as well,
+	so leaving as a quick implementation for now.
+	*/
+	let currentPage = 1;
+	let hasMore = true;
+	let isLoadingMore = false;
+	let invisibleFooter: HTMLDivElement;
+
+	// Initialize the IntersectionObserver for infinite scrolling
+	onMount(() => {
+		if (browser) {
+			type HandleIntersect = (entries: IntersectionObserverEntry[]) => void;
+
+			const handleIntersect: HandleIntersect = (entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting && hasMore && !isLoadingMore) {
+						loadMore();
+					}
+				});
+			};
+
+			const observer = new IntersectionObserver(handleIntersect, { threshold: 0.1 });
+			observer.observe(invisibleFooter);
+		}
+	});
+
+	/**
+	 * Fetches and appends more outcomes and intentions from the server.
+	 * If no new data is found, sets `hasMore` to false.
+	 */
+	async function loadMore() {
+		isLoadingMore = true;
+		const limit = 15;
+		const offset = currentPage * limit;
+
+		// Fetch new outcomes
+		const newOutcomes = await trpc($page).outcomes.list.query({
+			limit,
+			offset,
+			order: 'desc',
+			orderBy: 'date'
+		});
+
+		// Fetch new intentions if there are new outcomes
+		if (newOutcomes.length) {
+			data.outcomes = [...data.outcomes, ...newOutcomes];
+			const uniqueDatesResult = await trpc($page).intentions.listUniqueDates.query({
+				limit,
+				offset
+			});
+			const uniqueDates = uniqueDatesResult.map((d) => d.date);
+
+			if (uniqueDates.length) {
+				const [startDate, endDate] = [
+					new Date(uniqueDates[uniqueDates.length - 1]),
+					new Date(uniqueDates[0])
+				];
+				const newIntentionsByDate = await trpc($page).intentions.listByDate.query({
+					startDate,
+					endDate
+				});
+
+				// Merge the new intentions with existing ones
+				for (let date in newIntentionsByDate) {
+					data.intentionsByDate[date] = data.intentionsByDate[date]
+						? [...data.intentionsByDate[date], ...newIntentionsByDate[date]]
+						: newIntentionsByDate[date];
+				}
+
+				currentPage += 1;
+			} else {
+				hasMore = false;
+			}
+		} else {
+			hasMore = false;
+		}
+
+		isLoadingMore = false;
+	}
 </script>
 
 <svelte:head>
@@ -49,5 +137,17 @@
 				{/if}
 			{/each}
 		</div>
+		{#if isLoadingMore}
+			<Loader variant="bars" />
+		{/if}
+		<div bind:this={invisibleFooter} class="pagination-trigger"></div>
 	</section>
 {/if}
+
+<style>
+	.pagination-trigger {
+		height: 4px;
+		opacity: 0;
+		pointer-events: none;
+	}
+</style>
