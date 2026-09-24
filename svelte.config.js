dabbb941a7926dd24aa4fd76d446265440fd325b
Andrew <andrew@drewbitt.com>
2026-09-24 03:06:22 +0000

    chore(tooling): remove devbox, wire up lefthook, dedupe CI setup, swap deno adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,13 @@
 import nodeAdapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import denoAdapter from 'sveltekit-adapter-deno';
+import denoAdapter from '@deno/svelte-adapter';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter:
-			process.env.BUILD_ENV === 'deno'
-				? denoAdapter({
-						buildOptions: {
-							banner: {
-								js: `import { createRequire } from 'node:module';
-									import { fileURLToPath } from 'node:url';
-									import process from 'node:process';
-									import Buffer from 'node:buffer';
-									const require = createRequire(import.meta.url);
-									const __filename = fileURLToPath(import.meta.url);`
-							},
-							bundle: true
-						}
-					})
-				: nodeAdapter(),
+		adapter: process.env.BUILD_ENV === 'deno' ? denoAdapter() : nodeAdapter(),
 		alias: {
 			$src: './src',
 			$db: './src/lib/db'
